0490f5d97486f09d75d8ca8f4e515dd7c2d3d9bd
Shawn Wildermuth <shawn@wildermuth.com>
2022-06-29 19:08:14 -0400

    Contacts Saving Successfully

--- src/Tempus.App/src/fa/index.ts
+++ src/Tempus.App/src/fa/index.ts
@@ -1,12 +1,20 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import * as solid from '@fortawesome/free-solid-svg-icons'
-import * as brands from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import * as solid from "@fortawesome/free-solid-svg-icons";
+import * as brands from "@fortawesome/free-brands-svg-icons";
 
-export default function (): typeof FontAwesomeIcon  {
+export default function (): typeof FontAwesomeIcon {
   /* add icons to the library */
-  library.add(solid.faClock, solid.faStopwatch, solid.faSpinner, 
-              solid.faEye, solid.faXmark, solid.faPencil, solid.faCirclePlus);
+  library.add(
+    solid.faClock,
+    solid.faStopwatch,
+    solid.faSpinner,
+    solid.faEye,
+    solid.faXmark,
+    solid.faPencil,
+    solid.faCirclePlus,
+    solid.faFloppyDisk
+  );
   library.add(brands.faTwitter, brands.faTiktok, brands.faYoutube);
   return FontAwesomeIcon;
 }
